cef185372b996217c45034da68e86fcec07c18d9
shahbaz <shahbazpeerzade1800@gmail.com>
2023-04-10 13:58:39 +0530

    Initial commit

---
+++ src/main/java/com/batch/job/batch/job/config/SpringBatchConfig.java
@@ -0,0 +1,104 @@
+package com.batch.job.batch.job.config;
+
+import com.batch.job.batch.job.entity.Customer;
+import com.batch.job.batch.job.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+@AllArgsConstructor
+public class SpringBatchConfig {
+
+    private JobBuilder jobBuilder;
+    private StepBuilder stepBuilder;
+    private CustomerRepository customerRepository;
+
+    @Bean
+    public FlatFileItemReader<Customer> reader() {
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customer.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+
+    public LineMapper<Customer> lineMapper() {
+        DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id", "firstName", "lastName", "email", "gender", "contactNo", "country", "dob");
+
+        BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Customer.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+
+    }
+
+    @Bean
+    public CustomerProcessor processor() {
+        return new CustomerProcessor();
+    }
+
+    @Bean
+    public RepositoryItemWriter<Customer> writer() {
+        RepositoryItemWriter<Customer> writer = new RepositoryItemWriter<>();
+        writer.setRepository(customerRepository);
+        writer.setMethodName("save");
+        return writer;
+    }
+
+
+    JobRepository jobRepository;
+    private PlatformTransactionManager transactionManager;
+
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("csv-step", jobRepository)
+                .<Customer, Customer>chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+
+                .build();
+    }
+
+
+    @Bean
+    @Qualifier("pureRedDataProcessingJob")
+    public Job runJob() {
+        return new JobBuilder("importCustomers")
+                .flow(step1()).end().build();
+
+    }
+
+ /*   @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        asyncTaskExecutor.setConcurrencyLimit(10);
+        return asyncTaskExecutor;
+    }*/
+}
---
+++ src/main/java/com/batch/job/batch/job/controller/JobController.java
@@ -0,0 +1,34 @@
+package com.batch.job.batch.job.controller;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jobs")
+public class JobController {
+    @Autowired
+    JobLauncher jobLauncher;
+    Job job;
+
+    @PostMapping("importCustomers")
+    public void importScvToDbJob() {
+        JobParameters jobParameters=new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis())
+                .toJobParameters();
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobParametersInvalidException |
+                 JobInstanceAlreadyCompleteException | JobRestartException e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
